b1b04854cc88613bd284413b67104d749f235ba4
Mel <74751155+anotruong@users.noreply.github.com>
2022-10-07 14:49:51 -0700

    Got flash to work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const EXPRESS = require("express");
 //allows for flash to happen on pug
-const FLASH = require("express-flash");
+const flash = require("express-flash");
 const PG_PERSISTENCE = require("./lib/pg-persistence");
 const ERROR_CATCHER = require("./lib/error-catcher");
 //
@@ -18,35 +18,34 @@ app.set("view engine", "pug");
 
 //for express to find static images and css located in the directory "public"
 app.use(EXPRESS.static("public"));
+app.use(EXPRESS.urlencoded({extended: false}));
 
 app.use(SESSION({
-  cookie: {
-    httpOnly: true,
-    maxAge: 31 * 24 * 60 * 60 * 1000, //31 days in millseconds
-    path: "/",
-    secure: false,
-  },
-  name: "ingredient-alternatives-app",
+  // cookie: {
+  //   httpOnly: true,
+  //   maxAge: 31 * 24 * 60 * 60 * 1000, //31 days in millseconds
+  //   path: "/",
+  //   secure: false,
+  // },
+  name: "pantry-app",
   resave: false,
   saveUnintialized: true,
   secret: "potatos can make soba",
   store: new LokiStore({}),
 }));
 
+app.use(flash());
+
 app.use((req, res, next) => {
   res.locals.store = new PG_PERSISTENCE(req.SESSION);
   next();
 });
 
-// Temporary test code
-// app.use(async(req, res, next) => {
-//   try {
-//     await res.locals.store.displayAll();
-//     res.send("quitting");
-//   } catch(error) {
-//     next(error);
-//   }
-// });
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+})
 
 //home page
 app.get("/", (req, res) => {
@@ -57,12 +56,11 @@ app.get("/", (req, res) => {
 app.get("/home",
     async (req, res, next) => {
       try {
-        let ingredients_lists = await res.locals.store.displayAll();
-        // let ingre_name = ingredients_lists.map(obj => {
-          
-        // })
+        let ingredientsList = await res.locals.store.displayAll();
+        ingredientsList = await res.locals.store.valuesOf(ingredientsList);
+
         res.render("lists", {
-          ingredientsList: ingredients_lists,
+          ingredientsList,
         });
       } catch(error) {
         next(error)
@@ -72,18 +70,36 @@ app.get("/home",
 
 app.post("/home", 
   async (req, res, next) => {
-    try {
-      let ingredient_list = res.locals.store.displayAll();
 
-      res.render("list", {
-        ingredientsList: ingredient_list
-      });
-    } catch (error) {
-      next(error);
-    }
 })
 
+app.get("/newMain", (req, res) => {
+  // let tester = flash.
+  res.render("newMain");
+})
 
+//page for entering information for a new ingredient
+//will need to produce an error if the ingredient name is not unique
+app.post("/newMain", 
+  async(req, res, next) => {
+    try {
+      let name = req.body.nameMain.toLowerCase().trim();
+      let verifyUnique = await res.locals.store.isUnique(name)
+
+      if (!verifyUnique) {
+        req.flash("error", `${name} already exists in the database.`)
+        res.render("newMain", {
+          flash: req.flash(),
+        })
+      } else {
+        await res.locals.store.addItem(name);
+        req.flash("success", `${name} has been succesfully added!`);
+        res.redirect("/home");
+      }
+    } catch(error) {
+      next(error)
+    }
+});
 
 app.listen(PORT, "localhost", () => {
   console.log(`Listening to port ${PORT}`);
